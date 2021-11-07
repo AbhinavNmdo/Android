7318a0a66d3cda1df155add8cb243acc2fb6e593
Abhinav Namdeo <abhaynam22@gmail.com>
2021-11-07 14:02:22 +0530

    MultiScreen app completed

--- MultiScreen/app/src/main/java/com/example/multiscreen/MainActivity.java
+++ MultiScreen/app/src/main/java/com/example/multiscreen/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.multiscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText name;
+    EditText phone;
+    EditText email;
+    public static final String EXTRA_NAME = "com.example.multiscreen.EXTRA.NAME";
+    public static final String EXTRA_EMAIL = "com.example.multiscreen.EXTRA.EMAIL";
+    public static final String EXTRA_PHONE = "com.example.multiscreen.EXTRA.PHONE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void openActivity(View v){
+        Toast.makeText(this, "Opening New activity", Toast.LENGTH_SHORT).show();
+
+        name = findViewById(R.id.name);
+        phone = findViewById(R.id.phone);
+        email = findViewById(R.id.email);
+
+        String nameText = name.getText().toString();
+        String phoneText = phone.getText().toString();
+        String emailText = email.getText().toString();
+
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra(EXTRA_NAME, nameText);
+        intent.putExtra(EXTRA_EMAIL, emailText);
+        intent.putExtra(EXTRA_PHONE, phoneText);
+
+        startActivity(intent);
     }
 }
---
+++ MultiScreen/app/src/main/java/com/example/multiscreen/MainActivity2.java
@@ -0,0 +1,32 @@
+package com.example.multiscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView name2;
+    TextView phone2;
+    TextView email2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        name2 = findViewById(R.id.name2);
+        phone2 = findViewById(R.id.phone2);
+        email2 = findViewById(R.id.email2);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(MainActivity.EXTRA_NAME);
+        String phone = intent.getStringExtra(MainActivity.EXTRA_PHONE);
+        String email = intent.getStringExtra(MainActivity.EXTRA_EMAIL);
+
+        name2.setText(name);
+        phone2.setText(phone);
+        email2.setText(email);
+    }
+
+}
